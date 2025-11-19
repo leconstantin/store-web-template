92d94c94f8160be07a458adf9eb9eff0fbc62213
Leo Constantin <lcon69184@gmail.com>
2025-11-19 13:19:32 +0200

    chnaged landing og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { baseUrl } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: {
-    default: `${siteConfig.name}: Websites Design & Development Solutions`,
-    template: `%s | ${siteConfig.name}: Web Design & Development Solutions in Rwanda`,
+    default: `${siteConfig.name}`,
+    template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
   keywords: siteConfig.keywords,
@@ -19,14 +19,31 @@ export const metadata: Metadata = {
     },
   ],
   creator: siteConfig.name,
-
-  robots: {
-    follow: true,
-    index: true,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: `${siteConfig.name} - Web Design & Development in Rwanda`,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${siteConfig.name} | Web Design, Development & Digital Solutions`,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@rathonagency",
   },
   metadataBase: new URL(baseUrl),
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
